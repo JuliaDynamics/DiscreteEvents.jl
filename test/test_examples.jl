17bc693abf6e89b7a182770830e6692bc200cbae
Paul Bayer <Paul.Bayer@gleichsam.de>
2020-03-20 11:48:42 +0100

    include examples with testing

---
+++ test/test_examples.jl
@@ -0,0 +1,22 @@
+println(".... run examples and notebooks ....")
+
+using NBInclude
+
+dir = pwd()
+cd("examples")
+temp = pwd()
+
+ex = [x for x in readdir("src") if occursin(r".jl", x)]
+nb = [x for x in readdir("nb") if occursin(r".ipynb", x)]
+
+for x in ex
+    println("... include examples/src/$x:")
+    include(temp*"/src/"*x)
+end
+
+for x in nb
+    println("... @nbinclude examples/nb/$x:")
+    @nbinclude(temp*"/nb/"*x)
+end
+
+cd(dir)
