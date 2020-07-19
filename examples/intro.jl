c37a1d5001e9c197e68a253e1dbf734c48ee2fae
Paul Bayer <Paul.Bayer@gleichsam.de>
2020-07-19 16:54:17 +0200

    update intro example
    see issue #24

---
+++ examples/intro.jl
@@ -0,0 +1,26 @@
+#
+# introductory example in README.md
+#
+using DiscreteEvents, Printf, Random
+
+function simple(c::Clock, input::Channel, output::Channel, name, id, op)
+    token = take!(input)         # take something from the input
+    now!(c, ()->@printf("%5.2f: %s %d took token %d\n", tau(c), name, id, token))
+    d = delay!(c, rand())        # after a delay
+    put!(output, op(token, id))  # put it out with some op applied
+end
+
+clk = Clock()      # create a clock
+Random.seed!(123)  # seed the random number generator
+
+ch1 = Channel(32)  # create two channels
+ch2 = Channel(32)
+
+for i in 1:2:8     # create and register 8 SimProcesses SP
+    process!(clk, Prc(i, simple, ch1, ch2, "foo", i, +))
+    process!(clk, Prc(i+1, simple, ch2, ch1, "bar", i+1, *))
+end
+
+put!(ch1, 1)       # put first token into channel 1
+yield()            # let the first task take it
+run!(clk, 10)      # and run for 10 time units
