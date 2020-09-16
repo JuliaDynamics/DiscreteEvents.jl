cf5fa1658d1e93b3b4dcc61bc3444f762365e1f4
Paul Bayer <Paul.Bayer@gleichsam.de>
2020-09-16 16:23:18 +0200

    update macros
    see issue #29 and PR #30

--- src/macros.jl
+++ src/macros.jl
@@ -23,7 +23,7 @@ Wrap a function and its arguments in a [`Prc`](@ref) and start it with
 - an `Int` process id.
 """
 macro process(expr, args...)
-    expr.head != :call && error("Expression is not a function call.")
+    @assert expr isa Expr && expr.head == :call "1st term is not a function call!"
     f = expr.args[1] #extract function passed
     c = expr.args[2] #first function arg must be an AbstractClock
     fargs = expr.args[3:end] #extract other function args
@@ -48,7 +48,7 @@ Schedule a function `f(arg...)` as an event to a clock.
 - `n`: passed as keyword `n` to `event!`.
 """
 macro event(expr, args...)
-    expr.head != :call && error("1st term is not a function call.")
+    @assert expr isa Expr && expr.head == :call "1st term is not a function call!"
     f = expr.args[1] #extract function passed
     c = expr.args[2] #first function arg must be an AbstractClock
     fargs = expr.args[2:end] #extract other function args
@@ -61,8 +61,10 @@ macro event(expr, args...)
 end
 
 """
-    @event f(farg...) c(carg...)
-
+```
+@event f(farg...) c(carg...)
+@event f(farg...) ca
+```
 Schedule a function `f(farg...)` as a conditional event to a clock.
 
 # Arguments
@@ -70,19 +72,53 @@ Schedule a function `f(farg...)` as a conditional event to a clock.
 - `farg...`: its arguments, the first argument must be a clock,
 - `c`: function to be evaluated at the clock's sample rate, if it
     returns true, the event is triggered,
-- `carg...`: arguments to c.
+- `carg...`: arguments to c,
+- `ca`: an anyonymous function of the form `()->...` to be evaluated 
+    at the clock's sample rate, if it returns true, the event
+    is triggered.
 """
 macro event(expr, cond)
-    expr.head != :call && error("1st term is not a function call.")
+    @assert expr isa Expr && expr.head == :call "1st term is not a function call!"
     f1 = expr.args[1] 
     c = expr.args[2] 
     f1args = expr.args[2:end] 
     ex1 = :(fun($f1, $(f1args...))) 
-    cond.head != :call && error("2nd term is not a function call.")
-    f2 = cond.args[1] 
-    f2args = cond.args[2:end] 
-    ex2 = :(fun($f2, $(f2args...))) 
-    esc(:(event!($c, $ex1, $ex2)))
+    if cond isa Expr 
+        if cond.head == :call
+            f2 = cond.args[1] 
+            f2args = cond.args[2:end] 
+            ex2 = :(fun($f2, $(f2args...))) 
+        elseif cond.head == :->
+            ex2 = :(()->$(cond.args...))
+        end
+        esc(:(event!($c, $ex1, $ex2)))
+    else    # assume it should be a simple timed event
+        esc(:(event!($c, $ex1, $cond))) 
+    end
+end
+
+"""
+    @periodic f(arg...) [Δt]
+
+Register a function `f(arg...)` for periodic execution at the 
+clock`s sample rate.
+
+# Arguments
+- `f`: function to be executed periodically,
+- `arg...`: its arguments, the first argument must be a clock,
+- `Δt`: for setting the clock's sample rate.
+"""
+macro periodic(expr, args...)
+    @assert expr isa Expr && expr.head == :call "1st term is not a function call!"
+    f = expr.args[1]
+    c = expr.args[2]
+    fargs = expr.args[2:end]
+    ex = :(fun($f, $(fargs...)))
+    if length(args) == 3 
+        esc(:(periodic!($c, $ex, $(args[1:2]...), $(args[3])))) #execute event!
+    else
+        esc(:(periodic!($c, $ex, $(args...)))) #execute event!
+    end
 end
 
 """
