a00a53dd2dcad84a88644e1684c2f1a3898231ae
Paul Bayer <Paul.Bayer@gleichsam.de>
2021-03-18 10:44:33 +0100

    put repo link in documentation

--- src/DiscreteEvents.jl
+++ src/DiscreteEvents.jl
@@ -12,6 +12,8 @@
 A Julia package for generating and simulating discrete events. It runs on Julia
 `VERSION ≥ v"1.0"`. Multithreading requires `VERSION ≥ v"1.3"`.
 
+Its Github repo is [https://github.com/pbayer/DiscreteEvents.jl].
+
 The current stable, registered version is installed with
 ```julia
 pkg> add DiscreteEvents
